6e94f92dfc42104d1e4aec96555ff505bd6b1596
kickit-tours <168971946+kickit-tours@users.noreply.github.com>
2024-05-05 20:54:56 +0200

    xcx

--- VideoBackground.js
+++ VideoBackground.js
@@ -1,23 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './VideoBackground.css'; // Import your CSS file for styling
 
 const VideoBackground = () => {
-  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
-  const videos = ["./video1.mp4", "./video2.mp4"];
-  const totalVideos = videos.length;
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentVideoIndex((prevIndex) => (prevIndex + 1) % totalVideos);
-    }, videos[currentVideoIndex].duration * 1000); // Switch to next video after current video ends
-
-    return () => clearInterval(interval);
-  }, [currentVideoIndex, totalVideos, videos]);
-
   return (
-    <div> // className="video-background">
-      <video autoPlay loop muted > // playsInline
-        <source src={videos[currentVideoIndex]} type="video/mp4" />
+    <div className="video-background">
+      <video autoPlay loop muted playsInline>
+        <source src="./video1.mp4" type="video/mp4" />
+        {/* Add additional source elements for different video formats */}
         Your browser does not support the video tag.
       </video>
     </div>
