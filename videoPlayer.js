9d6b448d996bf4be605cae8481fcb7b4b36c9e8e
kickit-tours <168971946+kickit-tours@users.noreply.github.com>
2024-05-06 19:13:24 +0200

    zrtrz

--- videoPlayer.js
+++ videoPlayer.js
@@ -1,35 +1,36 @@
 // videoPlayer.js
 function videoPlayer(videoSources) {
-    const video = document.getElementById('background-video');
+    const videos = document.querySelectorAll('.video');
     let currentVideoIndex = 0;
   
-    // Preload the first video
-    video.src = videoSources[currentVideoIndex];
-    video.load();
+    videos.forEach((video, index) => {
+      // Preload the video
+      video.src = videoSources[index];
+      video.load();
   
-    // Play the video when loaded to avoid the first visible hiccup
-    video.oncanplay = () => {
-      video.play().catch(error => {
-        console.error('Failed to play video:', error);
-      });
-    };
+      // Play the video when loaded to avoid the first visible hiccup
+      video.oncanplay = () => {
+        video.play().catch(error => {
+          console.error('Failed to play video:', error);
+        });
+      };
   
-    // Listen for the 'ended' event to switch to the next video
-    video.addEventListener('ended', () => {
-      console.log('Video ended'); // Check if this message is logged
-      currentVideoIndex = (currentVideoIndex + 1) % videoSources.length;
-      video.src = videoSources[currentVideoIndex];
-      video.play().catch(error => {
-        console.error('Failed to play video:', error);
+      // Listen for the 'ended' event to switch to the next video
+      video.addEventListener('ended', () => {
+        currentVideoIndex = (currentVideoIndex + 1) % videos.length;
+        const nextVideo = videos[currentVideoIndex];
+        nextVideo.src = videoSources[currentVideoIndex];
+        nextVideo.play().catch(error => {
+          console.error('Failed to play video:', error);
+        });
       });
     });
   
-    // Log whether the 'ended' event listener is added successfully
-    console.log('Event listener added successfully:', video.getAttribute('onended') !== null);
-  
-    // Mute the video and hide controls
-    video.muted = true;
-    video.controls = false;
+    // Mute the videos and hide controls
+    videos.forEach(video => {
+      video.muted = true;
+      video.controls = false;
+    });
   }
   
   window.videoPlayer = videoPlayer;
