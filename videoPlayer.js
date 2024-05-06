7ede171a2fb5640ab60d4784bece4e4e3a590f2d
kickit-tours <168971946+kickit-tours@users.noreply.github.com>
2024-05-06 18:58:09 +0200

    iziuguziuz

--- videoPlayer.js
+++ videoPlayer.js
@@ -1,38 +1,36 @@
 // videoPlayer.js
-document.addEventListener('DOMContentLoaded', function() {
-    const videoContainer = document.getElementById('video-container');
+function videoPlayer(videoSources) {
+    const video = document.getElementById('background-video');
     let currentVideoIndex = 0;
   
-    console.log('hello');
-
-    // Listen for clicks on the video container
-    videoContainer.addEventListener('ended', function(event) {
-        console.log('event ended ', event.target.src);
-        // Check if the ended event originated from a video element
-      if (event.target.tagName.toLowerCase() === 'video') {
-        currentVideoIndex = (currentVideoIndex + 1) % videoSources.length;
-        const video = event.target;
-        video.src = videoSources[currentVideoIndex];
-        video.play().catch(error => {
-          console.error('Failed to play video:', error);
-        });
-      }
-    });
-
     // Preload the first video
-    const video = document.getElementById('background-video');
     video.src = videoSources[currentVideoIndex];
     video.load();
-
+  
     // Play the video when loaded to avoid the first visible hiccup
     video.oncanplay = () => {
-        video.play().catch(error => {
+      video.play().catch(error => {
         console.error('Failed to play video:', error);
       });
     };
   
+    // Listen for the 'ended' event to switch to the next video
+    video.addEventListener('ended', () => {
+      console.log('Video ended'); // Check if this message is logged
+      currentVideoIndex = (currentVideoIndex + 1) % videoSources.length;
+      video.src = videoSources[currentVideoIndex];
+      video.play().catch(error => {
+        console.error('Failed to play video:', error);
+      });
+    });
+  
+    // Log whether the 'ended' event listener is added successfully
+    console.log('Event listener added successfully:', video.getAttribute('onended') !== null);
+  
     // Mute the video and hide controls
     video.muted = true;
     video.controls = false;
-  });
+  }
+  
+  window.videoPlayer = videoPlayer;
   
