59e257cedfad1f49175428f51ad5f9e738bfe8f2
kickit-tours <168971946+kickit-tours@users.noreply.github.com>
2024-05-06 18:36:19 +0200

    jhkuh

--- videoPlayer.js
+++ videoPlayer.js
@@ -3,10 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const videoContainer = document.getElementById('video-container');
     let currentVideoIndex = 0;
   
-    console.log('hello 1');
+    console.log('hello');
+
     // Listen for clicks on the video container
     videoContainer.addEventListener('ended', function(event) {
-      // Check if the ended event originated from a video element
+        console.log('event ended ', event.target.src);
+        // Check if the ended event originated from a video element
       if (event.target.tagName.toLowerCase() === 'video') {
         currentVideoIndex = (currentVideoIndex + 1) % videoSources.length;
         const video = event.target;
@@ -17,18 +19,13 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
 
-    console.log('hello 2');
-
     // Preload the first video
     const video = document.getElementById('background-video');
     video.src = videoSources[currentVideoIndex];
     video.load();
 
-    console.log('hello 3');
-
     // Play the video when loaded to avoid the first visible hiccup
     video.oncanplay = () => {
-        console.log('hello 4');
         video.play().catch(error => {
         console.error('Failed to play video:', error);
       });
