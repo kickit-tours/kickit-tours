d7c0a57aeeec7c0fc0791b25ea0225fa8fa8e202
kickit-tours <168971946+kickit-tours@users.noreply.github.com>
2024-05-06 16:46:49 +0200

    grgr

--- videoPlayer.js
+++ videoPlayer.js
@@ -9,13 +9,9 @@ const videoPlayer = (videoElementId, videoSources) => {
     let currentSourceIndex = 0;
     let nextVideo = document.createElement('video');
     nextVideo.src = videoSources[currentSourceIndex + 1]; // Preload the next video
-    nextVideo.muted = true; // Mute the next video
+    nextVideo.muted = true;
   
     // Hide video controls
-    video.controls = false;
-  
-    let isFirstPlay = true;
-  
     const playNextVideo = () => {
       if (currentSourceIndex >= videoSources.length) {
         currentSourceIndex = 0; // Loop back to the first video
@@ -24,28 +20,25 @@ const videoPlayer = (videoElementId, videoSources) => {
       video.src = videoSources[currentSourceIndex];
       video.muted = true; // Mute the video
       video.load(); // Load the current video
+      video.controls = false;
   
       // Wait for the 'loadedmetadata' event before playing the video
       video.addEventListener('loadedmetadata', () => {
-        // Play the video only if it's not the first time
-        if (!isFirstPlay) {
           video.play().catch(error => {
             console.error('Failed to play video:', error);
           });
-        } else {
-          isFirstPlay = false;
-        }
       }, { once: true });
   
       // Preload the next video
       const nextSourceIndex = (currentSourceIndex + 1) % videoSources.length;
       nextVideo.src = videoSources[nextSourceIndex];
       nextVideo.load();
+      nextVideo.muted = true; 
       
       currentSourceIndex++;
     };
   
-    // Play the next video
+    // Play the first video
     playNextVideo();
   
     // Listen for the 'ended' event to play the next video
