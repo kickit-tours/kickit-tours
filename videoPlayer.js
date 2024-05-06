85e44bb028a01ae72a188328fc7d45b52ac04e1b
kickit-tours <168971946+kickit-tours@users.noreply.github.com>
2024-05-06 15:58:41 +0200

    schwede

--- videoPlayer.js
+++ videoPlayer.js
@@ -9,6 +9,7 @@ const videoPlayer = (videoElementId, videoSources) => {
     let currentSourceIndex = 0;
     let nextVideo = document.createElement('video');
     nextVideo.src = videoSources[currentSourceIndex + 1]; // Preload the next video
+    nextVideo.muted = true; // Mute the next video
   
     const playNextVideo = () => {
       if (currentSourceIndex >= videoSources.length) {
@@ -22,10 +23,22 @@ const videoPlayer = (videoElementId, videoSources) => {
       });
   
       // Preload the next video
-      nextVideo.src = videoSources[(currentSourceIndex + 1) % videoSources.length];
+      const nextSourceIndex = (currentSourceIndex + 1) % videoSources.length;
+      nextVideo.src = videoSources[nextSourceIndex];
       nextVideo.load();
       
       currentSourceIndex++;
+  
+      // Crossfade transition
+      video.style.transition = 'opacity 1s ease-out';
+      video.style.opacity = 1;
+      nextVideo.style.transition = 'opacity 1s ease-out';
+      nextVideo.style.opacity = 0;
+  
+      setTimeout(() => {
+        video.style.opacity = 0;
+        nextVideo.style.opacity = 1;
+      }, 1000); // Crossfade duration
     };
   
     // Play the first video
