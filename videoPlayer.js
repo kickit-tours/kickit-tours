01849f5147825356d37437aa1fc3031850de2c52
kickit-tours <168971946+kickit-tours@users.noreply.github.com>
2024-05-07 12:54:39 +0200

    fdhgjh

--- videoPlayer.js
+++ videoPlayer.js
@@ -2,18 +2,22 @@
 function videoPlayer(videoSources) {
     const videos = document.querySelectorAll('.video');
     let currentVideoIndex = 0;
-  
+    let isFirst = true;
+
     videos.forEach((video, index) => {
       // Preload the video
       video.src = videoSources[index];
       video.load();
   
       // Play the video when loaded to avoid the first visible hiccup
-      video.oncanplay = () => {
-        video.play().catch(error => {
-          console.error('Failed to play video:', error);
-        });
-      };
+      if(isFirst) {
+        video.oncanplay = () => {
+            video.play().catch(error => {
+              console.error('Failed to play video:', error);
+            });
+          };
+        isFirst = false;    
+      }
   
       // Listen for the 'ended' event to switch to the next video
       video.addEventListener('ended', () => {
