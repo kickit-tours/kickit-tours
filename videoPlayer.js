30913262727a8053c1b0a9966dde7af563d656df
kickit-tours <168971946+kickit-tours@users.noreply.github.com>
2024-05-07 20:51:11 +0200

    ztrzt

--- videoPlayer.js
+++ videoPlayer.js
@@ -18,7 +18,7 @@ function videoPlayer(videoSources) {
 
         // Play the video when loaded to avoid the first visible hiccup
         if (video.id === 'video1') {
-            video.addEventListener('progress', getPercentProg(video), false);
+//            video.addEventListener('progress', getPercentProg(video), false);
             video.addEventListener('canplaythrough', () => {
                 video.play().catch(error => {
                   console.error('Failed to play video:', error);
