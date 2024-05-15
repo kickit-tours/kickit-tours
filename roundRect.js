2b3c0531ac6b09ac31629fcda9c3d80a848f0635
kickit-tours <168971946+kickit-tours@users.noreply.github.com>
2024-05-15 17:34:01 +0200

    nbmnbm

--- roundRect.js
+++ roundRect.js
@@ -4,6 +4,8 @@ function roundRect(id, x, y, w, h, radius, rotation, transX, transY)
   var context = canvas.getContext("2d");
   var r = x + w;
   var b = y + h;
+  context.font = "30px Arial";
+  context.fillText("Hello", 10, 30);
   context.beginPath();
   context.rotate(rotation * Math.PI / 180);
 //  context.translate(transX, transY);
