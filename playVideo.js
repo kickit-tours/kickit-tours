c091b49725e452ffc15dd92bc28d1dcc18691cfb
kickit-tours <168971946+kickit-tours@users.noreply.github.com>
2024-05-18 13:19:17 +0200

    ghfhg

--- playVideo.js
+++ playVideo.js
@@ -18,7 +18,7 @@ function isMobileDevice() {
 // Set the video source based on the platform
 if (isSafariOniOS()) {
     video.src = mobileVideo; // Use mobile video for mobile devices and Safari on iOS
-    video.playbackRate = 1.6;
+    video.playbackRate = 2.0;
 } else {
     video.src = desktopVideo; // Use desktop video for other platforms
 }
